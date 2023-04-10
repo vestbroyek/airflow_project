a11d2908ae272550d79022554fa5566f79baab52
Maurits <mcb.westbroek@gmail.com>
2023-04-10 11:26:40 +0100

    Create LoadFactOperator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from psycopg2.errors import UniqueViolation
 
 class LoadFactOperator(BaseOperator):
 
@@ -8,15 +9,23 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                postgres_conn_id="postgres",
+                target_table="",
+                query="",
+                *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.postgres_conn_id=postgres_conn_id
+        self.target_table=target_table
+        self.query=query
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        postgres_hook=PostgresHook(self.postgres_conn_id)
+        with postgres_hook.get_conn() as conn:
+            with conn.cursor() as cursor:
+                # For fact table, insert
+                try:
+                    cursor.execute(f"""
+                    insert into {self.target_table} ({self.query})""")
+                except UniqueViolation as e:
+                    self.log.info(e)
