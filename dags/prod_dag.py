70fea4a5e23c242c7955c152035c1e622a308a33
Maurits <mcb.westbroek@gmail.com>
2023-04-10 11:27:16 +0100

    Implement first LoadFact and LoadDimension

--- dags/prod_dag.py
+++ dags/prod_dag.py
@@ -29,50 +29,66 @@ dag = DAG('prod_dag',
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
+    task_id='stage_events',
     dag=dag,
     key='log-data/2018-11-01-events.json',
     target_table='staging_events',
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
+    task_id='stage_songs',
     key='song-data/TRAAAAK128F9318786.json',
     target_table='staging_songs',
     dag=dag
 )
 
-# load_songplays_table = LoadFactOperator(
-#     task_id='Load_songplays_fact_table',
-#     dag=dag
-# )
+load_songplays_table = LoadFactOperator(
+    task_id='load_songplays_fact_table',
+    target_table="songplays",
+    query=SqlQueries.songplay_table_insert,
+    dag=dag
+)
 
-# load_user_dimension_table = LoadDimensionOperator(
-#     task_id='Load_user_dim_table',
-#     dag=dag
-# )
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='load_user_dim_table',
+    target_table="users",
+    query=SqlQueries.user_table_insert,
+    truncate=False,
+    dag=dag
+)
+
+start_operator >> (stage_events_to_redshift, stage_songs_to_redshift) >> load_songplays_table >> (load_user_dimension_table) # >> run_quality_checks >> end_operator
 
 # load_song_dimension_table = LoadDimensionOperator(
-#     task_id='Load_song_dim_table',
+#     task_id='load_song_dim_table',
+#     target_table="songs",
+#     query=SqlQueries.song_table_insert,
+#     truncate=True,
 #     dag=dag
 # )
 
 # load_artist_dimension_table = LoadDimensionOperator(
-#     task_id='Load_artist_dim_table',
+#     task_id='load_artist_dim_table',
+#     target_table="artists",
+#     query=SqlQueries.artist_table_insert,
+#     truncate=True,
 #     dag=dag
 # )
 
 # load_time_dimension_table = LoadDimensionOperator(
-#     task_id='Load_time_dim_table',
+#     task_id='load_time_dim_table',
+#     target_table="time",
+#     query=SqlQueries.time_table_insert,
+#     truncate=True,
 #     dag=dag
 # )
 
 # run_quality_checks = DataQualityOperator(
-#     task_id='Run_data_quality_checks',
+#     task_id='run_data_quality_checks',
 #     dag=dag
 # )
 
 # end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-# # dependencies
+# dependencies
 # start_operator >> (stage_events_to_redshift, stage_songs_to_redshift) >> load_songplays_table >> (load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table) >> run_quality_checks >> end_operator
